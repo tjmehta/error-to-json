08537d2fb6e042983a2c913211467588f1ae1339
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2022-02-13 16:37:32 +0100

    Move `Error_prototype_toJSON()` method out of closure

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,30 @@ export default errToJSON
 
 const nonEnumerablePropsToCopy = ['code', 'errno', 'syscall']
 
+function Error_prototype_toJSON(this: Error) {
+  const json = {
+    // Add all enumerable properties
+    ...this,
+    // normal props
+    name: this.name,
+    message: this.message,
+    stack: this.stack,
+  }
+
+  nonEnumerablePropsToCopy.forEach((key) => {
+    // @ts-ignore
+    if (key in this) json[key] = this[key]
+  })
+
+  return JSON.parse(stringify(json))
+}
+
 export function errToJSON<T extends {}>(json: any): T {
   // @ts-ignore
   const {toJSON} = Error.prototype
 
   // @ts-ignore
-  Error.prototype.toJSON = function () {
-    const json = {
-      // Add all enumerable properties
-      ...this,
-      // normal props
-      name: this.name,
-      message: this.message,
-      stack: this.stack,
-    }
-
-    nonEnumerablePropsToCopy.forEach((key) => {
-      // @ts-ignore
-      if (key in this) json[key] = this[key]
-    })
-
-    return JSON.parse(stringify(json))
-  }
+  Error.prototype.toJSON = Error_prototype_toJSON
 
   // get error json
   // @ts-ignore
